382f38c0ba5d1ae11ae5d4bd1b7899f3b9305d13
Wang11artgital <104808648+Wang11artgital@users.noreply.github.com>
2022-06-23 18:08:47 +0800

    up

--- Game.js
+++ Game.js
@@ -1,9 +1,11 @@
-import * as THREE from '/libs/three137/three.module.js';
-import { GLTFLoader } from '/libs/three137/GLTFLoader.js';
-import { DRACOLoader } from '../../libs/three137/DRACOLoader.js';
-import { RGBELoader } from '/libs/three137/RGBELoader.js';
-import { OrbitControls } from '/libs/three137/OrbitControls.js';
-import { LoadingBar } from '/libs/LoadingBar.js';
+import * as THREE from './libs/three137/three.module.js';
+import { GLTFLoader } from './libs/three137/GLTFLoader.js';
+import { RGBELoader } from './libs/three137/RGBELoader.js';
+import { NPCHandler } from './NPCHandler.js';
+import { LoadingBar } from './libs/LoadingBar.js';
+import { Pathfinding } from './libs/pathfinding/Pathfinding.js';
+import { User } from './User.js';
+import { Controller } from './Controller.js';
 
 class Game{
 	constructor(){
@@ -15,42 +17,112 @@ class Game{
         this.loadingBar = new LoadingBar();
         this.loadingBar.visible = false;
 
-		this.assetsPath = '/assets/';
+		this.assetsPath = './assets/';
         
-		this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 50 );
-		this.camera.position.set( 1, 1.7, 2.8 );
-        
-		let col = 0x605550;
+		this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 500 );
+		this.camera.position.set( -10.6, 1.6, -1.46 );
+		this.camera.rotation.y = -Math.PI*0.5;
+
+		let col = 0x201510;
 		this.scene = new THREE.Scene();
 		this.scene.background = new THREE.Color( col );
-		
+		this.scene.fog = new THREE.Fog( col, 100, 200 );
+
 		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
 		this.scene.add(ambient);
 
         const light = new THREE.DirectionalLight();
-        light.position.set( 0.2, 1, 1 );
-			
+        light.position.set( 4, 20, 20 );
+		light.target.position.set(-2, 0, 0);
+		light.castShadow = true;
+		//Set up shadow properties for the light
+		light.shadow.mapSize.width = 1024; 
+		light.shadow.mapSize.height = 512; 
+		light.shadow.camera.near = 0.5; 
+		light.shadow.camera.far = 50;
+		const d = 30; 
+		light.shadow.camera.left = -d;
+		light.shadow.camera.bottom = -d*0.25;
+		light.shadow.camera.right = light.shadow.camera.top = d;
+		this.scene.add(light);
+		this.light = light;
+	
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
+		this.renderer.shadowMap.enabled = true;
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
         this.renderer.outputEncoding = THREE.sRGBEncoding;
 		container.appendChild( this.renderer.domElement );
         this.setEnvironment();
-        
-        const controls = new OrbitControls( this.camera, this.renderer.domElement );
-        controls.target.set(0, 1, 0);
-		controls.update();
-
-        this.loadNPC();
 		
-		window.addEventListener('resize', this.resize.bind(this) );
-        
+		this.load();
+
+		this.raycaster = new THREE.Raycaster();
+		this.tmpVec = new THREE.Vector3();
+		
+		window.addEventListener( 'resize', this.resize.bind(this) );
+	}
+
+	seeUser(pos, seethrough=false){
+		if (this.seethrough){
+			this.seethrough.forEach( child => {
+				child.material.transparent = false;
+				child.material.opacity = 1;
+				//child.visible = true;
+			});
+			delete this.seethrough;
+		}
+
+		this.tmpVec.copy(this.user.position).sub(pos).normalize();
+		this.raycaster.set(pos, this.tmpVec);
+
+		const intersects = this.raycaster.intersectObjects(this.factory.children, true);
+		let userVisible = true;
+
+		if (intersects.length>0){
+			const dist = this.tmpVec.copy(this.user.position).distanceTo(pos);
+			
+			if (seethrough){
+				this.seethrough = [];
+				intersects.some( intersect => {
+					if (intersect.distance < dist){
+						this.seethrough.push(intersect.object);
+						//intersect.object.visible = false;
+						intersect.object.material.transparent = true;
+						intersect.object.material.opacity = 0.3;
+					}else{
+						return true;
+					}
+				})
+			}else{
+				userVisible = (intersects[0].distance > dist);
+			}
+			
+		}
+
+		return userVisible;
+	}
+
+	initPathfinding(navmesh){
+		this.waypoints = [
+			new THREE.Vector3(17.73372016326552, 0.39953298254866443, -0.7466724607286782),
+			new THREE.Vector3(20.649478054772402, 0.04232912113775987, -18.282935518174437),
+			new THREE.Vector3(11.7688416798274, 0.11264635905666916, -23.23102176233945),
+			new THREE.Vector3(-3.111551689570482, 0.18245423057147991, -22.687392486867505),
+			new THREE.Vector3(-13.772447796604245, 0.1260277454451636, -23.12237117145656),
+			new THREE.Vector3(-20.53385139415452, 0.0904175187063471, -12.467546107992108),
+			new THREE.Vector3(-18.195950790753532, 0.17323640676321908, -0.9593366354062719),
+			new THREE.Vector3(-6.603208729295872, 0.015786387893574227, -12.265553884212125)
+		];
+		this.pathfinder = new Pathfinding();
+        this.pathfinder.setZoneData('factory', Pathfinding.createZone(navmesh.geometry, 0.02));
+		if (this.npcHandler.gltf !== undefined) this.npcHandler.initNPCs();
 	}
 	
     resize(){
         this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize( window.innerWidth, window.innerHeight ); 
+    	this.camera.updateProjectionMatrix();
+    	this.renderer.setSize( window.innerWidth, window.innerHeight ); 
     }
     
     setEnvironment(){
@@ -58,42 +130,56 @@ class Game{
         const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
         pmremGenerator.compileEquirectangularShader();
         
-        const self = this;
-        
-        loader.load( 'hdr/factory.hdr', ( texture ) => {
-            const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
-            pmremGenerator.dispose();
+        loader.load( 'hdr/factory.hdr', 
+		texture => {
+          const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
+          pmremGenerator.dispose();
 
-            self.scene.environment = envMap;
+          this.scene.environment = envMap;
 
-        }, undefined, (err)=>{
+		  this.loadingBar.visible = !this.loadingBar.loaded;
+        }, 
+		xhr => {
+			this.loadingBar.update( 'envmap', xhr.loaded, xhr.total );
+		},
+		err => {
             console.error( err.message );
         } );
     }
+    
+	load(){
+        this.loadEnvironment();
+		this.npcHandler = new NPCHandler(this);
+		this.user = new User(this, new THREE.Vector3( -5.97, 0.021, -1.49), 1.57)
+    }
+
     loadEnvironment(){
-        const loader = new GLTFLoader( ).setPath(`${this.assetsPath}factory/`);
+    	const loader = new GLTFLoader( ).setPath(`${this.assetsPath}factory/`);
         
         this.loadingBar.visible = true;
 		
 		// Load a glTF resource
 		loader.load(
 			// resource URL
-			'factory1.glb',
+			'factory2.glb',
 			// called when the resource is loaded
 			gltf => {
+
 				this.scene.add( gltf.scene );
                 this.factory = gltf.scene;
 				this.fans = [];
+
 				const mergeObjects = {elements2:[], elements5:[], terrain:[]};
+
 				gltf.scene.traverse( child => {
 					if (child.isMesh){
-						if(child.name=='NavMesh'){
+						if (child.name == 'NavMesh'){
 							this.navmesh = child;
-							this.navmesh.geometry.rotateX(Math.PI/2);
+							this.navmesh.geometry.rotateX( Math.PI/2 );
 							this.navmesh.quaternion.identity();
 							this.navmesh.position.set(0,0,0);
-						}
-						else if (child.name.includes('fan')){
+							child.material.visible = false;
+						}else if (child.name.includes('fan')){
 							this.fans.push( child );
 						}else if (child.material.name.includes('elements2')){
 							mergeObjects.elements2.push(child);
@@ -115,6 +201,8 @@ class Game{
 					}
 				});
 
+				this.scene.add(this.navmesh);
+
 				for(let prop in mergeObjects){
 					const array = mergeObjects[prop];
 					let material;
@@ -127,9 +215,13 @@ class Game{
 					});
 				}
 
-                this.loadingBar.visible = false;
-                
+				this.controller = new Controller(this);
+
                 this.renderer.setAnimationLoop( this.render.bind(this) );
+
+				this.initPathfinding(this.navmesh);
+
+				this.loadingBar.visible = !this.loadingBar.loaded;
 			},
 			// called while loading is progressing
 			xhr => {
@@ -145,94 +237,23 @@ class Game{
 			}
 		);
 	}			
-    loadNPC(){
-        const loader = new GLTFLoader( ).setPath(`${this.assetsPath}factory/`);
-		const dracoLoader = new DRACOLoader();
-        dracoLoader.setDecoderPath( '/libs/three137/draco/' );
-        loader.setDRACOLoader( dracoLoader );
-        this.loadingBar.visible = true;
-		
-		// Load a glTF resource
-		loader.load(
-			// resource URL
-			'swat-guy.glb',
-			// called when the resource is loaded
-			gltf => {
-
-				this.scene.add( gltf.scene );
-                this.eve = gltf.scene;
-				this.mixer = new THREE.AnimationMixer( gltf.scene );
-
-				this.animations = {};
-
-				gltf.animations.forEach( animation => {
-					this.animations[animation.name.toLowerCase()] = animation;
-				});
-				
-				this.actionName = '';
-				this.newAnim();
-				
-                this.loadingBar.visible = false;
-                
-                this.renderer.setAnimationLoop( this.render.bind(this) );
-			},
-			// called while loading is progressing
-			xhr => {
-
-				this.loadingBar.progress = (xhr.loaded / xhr.total);
-				
-			},
-			// called when loading has errors
-			err => {
-
-				console.error( err );
-
-			}
-		);
-	}			
     
-	newAnim(){
-		const keys = Object.keys(this.animations);
-		let index;
-
-		do{
-			index = Math.floor( Math.random() * keys.length );
-		}while(keys[index]==this.actionName);
-
-		this.action = keys[index];
-
-		setTimeout( this.newAnim.bind(this), 3000 );
+	startRendering(){
+		this.renderer.setAnimationLoop( this.render.bind(this) );
 	}
 
-	set action(name){
-		if (this.actionName == name.toLowerCase()) return;
-				
-		const clip = this.animations[name.toLowerCase()];
-
-		if (clip!==undefined){
-			const action = this.mixer.clipAction( clip );
-			if (name=='shot'){
-				action.clampWhenFinished = true;
-				action.setLoop( THREE.LoopOnce );
-			}
-			action.reset();
-			const nofade = this.actionName == 'shot';
-			this.actionName = name.toLowerCase();
-			action.play();
-			if (this.curAction){
-				if (nofade){
-					this.curAction.enabled = false;
-				}else{
-					this.curAction.crossFadeTo(action, 0.5);
-				}
-			}
-			this.curAction = action;
-		}
-	}
 	render() {
 		const dt = this.clock.getDelta();
 
-		if (this.mixer !== undefined) this.mixer.update(dt);
+		if (this.fans !== undefined){
+            this.fans.forEach(fan => {
+                fan.rotateY(dt); 
+            });
+        }
+
+		if (this.npcHandler !== undefined ) this.npcHandler.update(dt);
+		if (this.user !== undefined ) this.user.update(dt);
+		if (this.controller !== undefined) this.controller.update(dt);
 
         this.renderer.render( this.scene, this.camera );
 
